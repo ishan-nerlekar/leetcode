79e6ba81fb017ed625826c9369c79d47a8820996
Ishan Nerlekar <78009247+Jigsxw@users.noreply.github.com>
2022-11-23 03:15:21 +0530

    Time: 50 ms (13.20%), Space: 43.9 MB (43.01%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> commonChars(String[] A) {
+        List<String> ans=new ArrayList<>();
+        
+        HashMap<Character,Integer> union=new HashMap<>();
+        
+        for(int i=0;i<A[0].length();i++){
+            union.put(A[0].charAt(i),union.getOrDefault(A[0].charAt(i),0)+1);
+        }
+        
+        for(int i = 1; i < A.length; i++){
+        HashMap<Character, Integer> temp = new HashMap<>(); 
+        for(int j = 0; j < A[i].length(); j++){
+            char curr = A[i].charAt(j); 
+            if(union.containsKey(curr)) temp.put(curr, Math.min(union.get(curr), temp.getOrDefault(curr, 0)+1)); 
+        }
+        union = temp; 
+    }
+        
+        for(char c: union.keySet()){
+            for(int i=0;i<union.get(c);i++){
+                ans.add(c+"");
+            }
+        }
+        
+        return ans;
+    }
+    
+}
