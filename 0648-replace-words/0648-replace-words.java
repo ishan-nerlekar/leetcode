cc6c9cedca1cbbd48b4242ce27c371396bbb4fea
Ishan Nerlekar <78009247+Jigsxw@users.noreply.github.com>
2023-01-13 01:00:28 +0530

    Time: 729 ms (17.65%), Space: 50.3 MB (97.10%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,27 +1,27 @@
 class Solution {
     public String replaceWords(List<String> dictionary, String sentence) {
-        String[] sen=sentence.split(" ");
+        String[] sen=sentence.split("\\s+");
         Set<String> set=new HashSet<>();
+        
+        if (dictionary == null || dictionary.size() == 0) return sentence;
         
         for(String str:dictionary){
             set.add(str);
         }
         
-        for(int j=0;j<sen.length;j++){
-            for(int i=0;i<sen[j].length();i++){
-                if(set.contains(sen[j].substring(0,i+1))){
-                    sen[j]=sen[j].substring(0,i+1);
+        StringBuilder sb=new StringBuilder();
+        
+        for(String s: sen){
+            String prefix="";
+            for(int i=0;i<s.length();i++){
+                prefix=s.substring(0,i+1);
+                if(set.contains(prefix)){
                     break;
                 }
             }
+            sb.append(" "+prefix);
         }
         
-        String myStr="";
-        for(String s:sen){
-            myStr=myStr+s+" ";
-        }
-        myStr=myStr.trim();
-        
-        return myStr;
+        return sb.deleteCharAt(0).toString();
     }
 }
