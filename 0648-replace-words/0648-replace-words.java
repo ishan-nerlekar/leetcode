e5db513828041cd39fd3e3ca9dd72a4ee11db417
Ishan Nerlekar <78009247+Jigsxw@users.noreply.github.com>
2023-01-13 00:38:50 +0530

    Time: 788 ms (12.51%), Space: 50.7 MB (92.23%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String replaceWords(List<String> dictionary, String sentence) {
+        String[] sen=sentence.split(" ");
+        Set<String> set=new HashSet<>();
+        
+        for(String str:dictionary){
+            set.add(str);
+        }
+        
+        for(int j=0;j<sen.length;j++){
+            for(int i=0;i<sen[j].length();i++){
+                if(set.contains(sen[j].substring(0,i+1))){
+                    sen[j]=sen[j].substring(0,i+1);
+                }
+            }
+        }
+        
+        String myStr="";
+        for(String s:sen){
+            myStr=myStr+s+" ";
+        }
+        myStr=myStr.trim();
+        
+        return myStr;
+    }
+}
