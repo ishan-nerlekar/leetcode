824f1f1ffaeb7c2cbb17eee0546dbaf46f6ec576
Ishan Nerlekar <78009247+Jigsxw@users.noreply.github.com>
2023-04-24 16:39:35 +0530

    Time: 5 ms (28.30%), Space: 7.6 MB (84.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,56 +1,17 @@
 class Solution {
 public:
-    void max_heapify(vector<int>& arr, int i, int size_)
-{
-    int largest, l = (2*i) + 1, r = l + 1;
-
-    if(l < size_ && arr[l] > arr[i])
-        largest = l;
-    else
-        largest = i;
-
-    if(r < size_ && arr[r] > arr[largest])
-        largest = r;
-
-    if(largest != i)
-    {
-        swap(arr[i], arr[largest]);
-        max_heapify(arr, largest, size_);
-    }
-}
-    
-    void build_max_heap(vector<int>& arr)
-{
-    for(int i = (arr.size() / 2); i >= 0; i--)
-    max_heapify(arr, i, arr.size());
-}
-
-void heap_sort(vector<int>& arr)
-{
-   build_max_heap(arr);
-   int sz = arr.size();
-   for(int i = arr.size() - 1; i > 0; i--)
-   {
-        std::swap(arr[0], arr[i]);
-        sz--;
-        max_heapify(arr, 0, sz);
-    }
-}
-    
-    int lastStoneWeight(vector<int>& vec) {
-        heap_sort(vec);
-    
-    while(vec.size()>1){
-		int x=vec.back();
-		vec.pop_back();
-		int y=vec.back();
-		vec.pop_back();
-		if(x>y) vec.push_back(x-y);
-		
-		heap_sort(vec);
-	}
-	int res=vec.empty()?0:vec.front();
-        
-        return res;
+    int lastStoneWeight(vector<int>& stones) {
+        sort(stones.begin(),stones.end());
+        while(stones.size()>1){
+            int x=stones.back();
+            stones.pop_back();
+            int y=stones.back();
+            stones.pop_back();
+            if(x>y){
+                stones.push_back(x-y);
+            }
+            sort(stones.begin(),stones.end());
+        }
+        return stones.empty()?0:stones.front();
     }
 };
